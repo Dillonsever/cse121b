f62ae5f53d3c28a6ccf55c6205df6be20ac1aa81
dillonsever <dillonsever@gmail.com>
2024-02-17 19:05:48 -0700

    projectt

---
+++ scripts/script.js
@@ -0,0 +1,36 @@
+
+function fetchData() {
+  fetch('https://bible-api.com/john+3:16')
+      .then(response => {
+          if (!response.ok) {
+              throw new Error('Network response was not ok');
+          }
+          return response.json();
+      })
+      .then(data => {
+          displayData(data);
+      })
+      .catch(error => {
+          console.error('There was a problem with your fetch operation:', error);
+          const dataContainer = document.getElementById('data-container');
+          dataContainer.innerHTML = '<p>Error: Failed to fetch data</p>';
+      });
+}
+
+function displayData(data) {
+  const dataContainer = document.getElementById('data-container');
+  dataContainer.innerHTML = '';
+
+  if (data.title && data.description) {
+      const itemHTML = `<div class="item">
+                          <h2>${data.title}</h2>
+                          <p>${data.description}</p>
+                      </div>`;
+      dataContainer.innerHTML = itemHTML;
+  } else {
+      const errorMessage = '<p>Error: Data format is not valid</p>';
+      dataContainer.innerHTML = errorMessage;
+  }
+}
+
+fetchData();
